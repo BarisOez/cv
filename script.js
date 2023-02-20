c47e1bdd3419738e9de51723abbbcf84813a2fdb
Baris Oezcan <112483726+BarisOez@users.noreply.github.com>
2023-02-20 10:07:08 +0100

    CV files

---
+++ script.js
@@ -0,0 +1,51 @@
+/*let js = 'amazing';
+
+console.log(40+ 8 + 23 - 10);
+
+console.log ("Jonas");
+
+let firstName = "Bob";
+let first_name_person = "Gugus"
+let ages = 10;
+console.log(firstName);
+console.log(first_name_person);
+console.log(ages); 
+
+
+true;
+
+let javascriptisfun = true;
+console.log(javascriptisfun);
+
+console.log(typeof true);
+console.log(typeof javascriptisfun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+javascriptisfun = "Yes";
+console.log(typeof javascriptisfun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+console.log(typeof null); 
+let age = 30;
+age = 31;
+const birthYear = 1991;
+console.log(birthYear);
+
+var job = "programmer";
+job = "teacher";*/
+const ageJonas = 2037-1992;
+console.log(ageJonas);
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = "Jonas";
+const LastName = "Schmedtmann";
+
+console.log(firstName + " " + LastName);
+
+console.log (ageJonas<=45)
